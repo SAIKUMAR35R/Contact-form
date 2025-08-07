99e3746cafa92b4622f10824335487f5244ca820
SAIKUMAR35R <2020cse.rf7@svce.edu.in>
2025-08-07 16:50:56 +0530

    added the message success flag on form submit and also used media queries to make the application layout for small screens.

--- src/app/contact-form.component.ts
+++ src/app/contact-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, ChangeDetectorRef } from "@angular/core";
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
@@ -6,9 +7,10 @@ import { FormGroup, FormControl, Validators, ReactiveFormsModule } from "@angula
   templateUrl: "./contact-form.component.html",
   styleUrls: ["./contact-form.component.css"],
   standalone: true,
-  imports: [ReactiveFormsModule]
+  imports: [ReactiveFormsModule, CommonModule]
 })
 export class ContactFormComponent {
+    constructor(private cdr: ChangeDetectorRef) {}
     contactForm = new FormGroup({
         firstName: new FormControl('', [Validators.required, Validators.minLength(3)]),
         lastName: new FormControl('', [Validators.required, Validators.minLength(3)]),
@@ -18,10 +20,25 @@ export class ContactFormComponent {
         consent: new FormControl(false, Validators.requiredTrue)
     });
     submitted = false;
+    showSuccess = false;
     onSubmit() {
         this.submitted = true;
         if (this.contactForm.valid) {
             console.log("Form Submitted!", this.contactForm.value);
+            this.showSuccess = true;
+
+            // Hide success banner after 3 seconds
+            setTimeout(() => {
+                this.showSuccess = false;
+                this.cdr.markForCheck();
+            }, 3000);
+
+            // Save message value
+            const messageValue = this.contactForm.get('message')?.value || '';
+
+            // Reset all fields
+            this.contactForm.reset();
+
         } else {
             console.log("Form is invalid");
         }
